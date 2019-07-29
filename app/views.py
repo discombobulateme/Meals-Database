406089fa327b2d3372edf7f6aab329193d312022
Shirley <shirleytaubersharon@gmail.com>
2019-07-29 20:01:15 +0200

    Added Meal Page Feature

--- app/views.py
+++ app/views.py
@@ -8,3 +8,11 @@ def index():
 	heading = "Frauenloop"
 	subheading = "Meals DB Coding Challenge"
 	return render_template('index.html', heading=heading, subheading=subheading)
+
+@app.route('/meal/<mealID>')
+def randos(mealID):
+    heading = "Meals"
+    subheading = "Accessing the Random User API"
+    url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=%s"%mealID
+    response = requests.request("GET", url)
+    return render_template('meal.html', heading=heading, subheading=subheading, food=response.json())
