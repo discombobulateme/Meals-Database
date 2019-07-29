06f1d35a541ab1b51ce47bc3bada532573a8dbfa
Genevieve Sabin <gev16ster@gmail.com>
2019-07-29 20:00:55 +0200

    we made meal categories

--- app/views.py
+++ app/views.py
@@ -8,3 +8,11 @@ def index():
 	heading = "Frauenloop"
 	subheading = "Meals DB Coding Challenge"
 	return render_template('index.html', heading=heading, subheading=subheading)
+
+@app.route('/categories/<categoriesID>')
+def randos(categoriesID):
+    heading = "categories"
+    subheading = "Many delicious meals"
+    url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=%s" % categoriesID
+    response = requests.request("GET", url)
+    return render_template('index.html', heading=heading, subheading=subheading, meal=response.json())
